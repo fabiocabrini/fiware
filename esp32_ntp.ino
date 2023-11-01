bb7f2d98e7a0cfbf707e86d7cd41470e65d0a815
Fábio Henrique Cabrini <fabio.cabrini@gmail.com>
2023-11-01 10:32:35 -0300

    Create esp32_ntp.ino

---
+++ esp32_ntp.ino
@@ -0,0 +1,44 @@
+#include <WiFi.h>
+
+#define NTP_SERVER     "IP_NTP_SERVER"
+#define UTC_OFFSET     0
+#define UTC_OFFSET_DST 0
+
+void printLocalTime() {
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("Connection Err");
+    return;
+  }
+
+  // Ajustar o deslocamento horário para UTC -3 (AST/BRT)
+  timeinfo.tm_hour -= 3;
+  if (timeinfo.tm_hour < 0) {
+    timeinfo.tm_hour += 24;  // Garantir que o valor de hora seja positivo
+  }
+
+  Serial.println(&timeinfo, "%H:%M:%S");
+  Serial.println(&timeinfo, "%d/%m/%Y   %Z");
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Connecting to ");
+  Serial.print("WiFi ");
+  WiFi.begin("SSID", "PASSWORD", 6);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(250);
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("Online");
+  Serial.println("Updating time...");
+  configTime(UTC_OFFSET, UTC_OFFSET_DST, NTP_SERVER);
+}
+
+void loop() {
+  printLocalTime();
+  delay(250);
+}
